44a24f7becbcfdec0a1ac19f5ede900c92df6eec
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-04-15 11:12:21 +0700

    Provider/Consumer header, stats, index, app

--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -3,4 +3,4 @@ import React from "react";
 const ScoreboardContext = React.createContext();
 
 export const Provider = ScoreboardContext.Provider;
-export const Consumner = ScoreboardContext.Consumer;
+export const Consumer = ScoreboardContext.Consumer;
