144217efb1984f0ee803ca63656b078f28723100
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2022-04-15 11:26:49 +0700

    Change Score function moved to Context

--- src/components/App.js
+++ src/components/App.js
@@ -64,14 +64,18 @@ class App extends Component {
 
   render() {
     return (
-      <Provider value={this.state.players}>
+      <Provider
+        value={{
+          players: this.state.players,
+          actions: {
+            changeScore: this.handleScoreChange,
+          },
+        }}
+      >
         <div className="scoreboard">
           <Header />
 
-          <PlayerList
-            changeScore={this.handleScoreChange}
-            removePlayer={this.handleRemovePlayer}
-          />
+          <PlayerList removePlayer={this.handleRemovePlayer} />
 
           <AddPlayerForm addPlayer={this.handleAddPlayer} />
         </div>
